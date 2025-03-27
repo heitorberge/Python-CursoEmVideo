6430f0da1e8da555cfce15c696284fabbf52cfe2
Heitor Berg <heitorberg114@gmail.com>
2025-03-27 09:40:14 -0300

    Update Desafio03.py

--- --Python (Curso Em video)--/Python-Mundo 1/Desafios/Desafio03.py
+++ --Python (Curso Em video)--/Python-Mundo 1/Desafios/Desafio03.py
@@ -1,5 +1,5 @@
 print('==== DESAFIO 03 ====')
-n1 = input('Primeiro Número: ')
-n2 = input('Segundo Número: ')
+n1 = int(input('Primeiro Número: '))
+n2 = int(input('Segundo Número: '))
 n3 = n1 + n2
-print('A soma dos dois números é: ' +  n3)
+print('A soma dos dois números é {}'.format(n3))
