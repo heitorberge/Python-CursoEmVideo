f81b53b97abf48399a6fd6174270f58322e6f8a1
Heitor Berg <heitorberg114@gmail.com>
2025-03-27 09:56:30 -0300

    Update Desafio03.py

--- --Python (Curso Em video)--/Python-Mundo 1/Desafios/Desafio03.py
+++ --Python (Curso Em video)--/Python-Mundo 1/Desafios/Desafio03.py
@@ -2,4 +2,4 @@ print('==== DESAFIO 03 ====')
 n1 = int(input('Primeiro Número: '))
 n2 = int(input('Segundo Número: '))
 n3 = n1 + n2
-print('A soma dos dois números é {}'.format(n3))
+print('A soma entre ', n1 ,' e ', n2 ,' é {}'.format(n3))
