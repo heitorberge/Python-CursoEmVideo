c774b6a295ae2f6829f76700b3c065330b168f2c
Heitor Berg <heitorberg114@gmail.com>
2025-04-04 08:45:58 -0300

    Add files via upload

---
+++ --Python (Curso Em video)--/Python-Mundo 1/PythonExercicios/ex001.py
@@ -0,0 +1,2 @@
+nome = input('Qual seu Nome?')
+print('É um grande prazer te conhecer ', nome)
---
+++ --Python (Curso Em video)--/Python-Mundo 1/PythonExercicios/ex011.py
@@ -0,0 +1,3 @@
+n = float(input('Quanto dinheiro você tem na carteira? R$'))
+d2 = n / 3.27
+print('Com R${:.2f} você pode comprar U${:.2f}'.format(n,d2))
