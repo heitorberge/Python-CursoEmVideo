3087e2933351d43400e4f64d8bd7a5b83f0af60a
Heitor Berg <heitorberg114@gmail.com>
2025-04-04 08:44:16 -0300

    Delete --Python (Curso Em video)--/Python-Mundo 1/PythonExercicios directory

--- --Python (Curso Em video)--/Python-Mundo 1/PythonExercicios/ex006.py
+++
@@ -1,2 +0,0 @@
-n1 = int(input('Digite um número:  '))
-print('Analisando o valor {}, seu antecessor é {} e o sucessor é {}'.format(n1,(n1 - 1),(n1 + 1)))
--- --Python (Curso Em video)--/Python-Mundo 1/PythonExercicios/ex012.py
+++
@@ -1,6 +0,0 @@
-lar = float(input('Largura da parede: '))
-alt = float(input('Altura da parede: '))
-area = lar * alt
-print('Sua Parede tem a dimensão de {}x{} e sua área é de {}m².'.format(lar,alt,area))
-tinta = area / 2
-print('Para pintar essa parede você precisará de {}l de tinta.'.format(tinta))
