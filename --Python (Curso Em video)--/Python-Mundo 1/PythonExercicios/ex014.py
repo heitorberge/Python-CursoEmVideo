f71db5000dc58651a09b19cd30470100102ad9d2
Heitor Berg <heitorberg114@gmail.com>
2025-04-05 10:10:19 -0300

    Delete --Python (Curso Em video)--/Python-Mundo 1/PythonExercicios directory

--- --Python (Curso Em video)--/Python-Mundo 1/PythonExercicios/ex014.py
+++
@@ -1,3 +0,0 @@
-slr = float(input('Digite o salário de um funcionário: R$'))
-aum = slr + (slr*15/100)
-print('Um funcionário que ganhava R${:.2f}, com 15% de aumento, passa a receber R${:.2f}'.format(slr,aum))
--- --Python (Curso Em video)--/Python-Mundo 1/PythonExercicios/ex016.py
+++
@@ -1,5 +0,0 @@
-print('-----ALUGUEL DE CARROS -----')
-dias = int(input('Quantos dias alugados? '))
-km = float(input('Quantos Km rodados? '))
-pago = (dias * 60) + (km * 0.15)
-print('O total a pagar é de R${:.2f}'.format(pago))
