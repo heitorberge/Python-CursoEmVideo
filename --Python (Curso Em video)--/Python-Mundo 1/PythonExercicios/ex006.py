829009eb31ca5bc85337c83cbfaee7330e9a8db8
Heitor Berg <heitorberg114@gmail.com>
2025-04-16 09:52:06 -0300

    Delete --Python (Curso Em video)--/Python-Mundo 1/PythonExercicios directory

--- --Python (Curso Em video)--/Python-Mundo 1/PythonExercicios/ex006.py
+++
@@ -1,5 +0,0 @@
-n1 = int(input('Digite um número: '))
-d = n1 * 2
-t = n1 * 3
-rq = n1 ** (1 / 2)
-print('O dobro de {} é {} \nO triplo de {} é {}\nA raiz quadrada de {} é {:.2f}'.format(n1, d, n1, t, n1, rq))
