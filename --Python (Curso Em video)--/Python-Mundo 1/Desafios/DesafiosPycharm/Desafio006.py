d2a4ef5650d3803a71ecd6a890479edfdbe23042
Heitor Berg <heitorberg114@gmail.com>
2025-04-16 09:53:16 -0300

    Delete --Python (Curso Em video)--/Python-Mundo 1/Desafios directory

--- --Python (Curso Em video)--/Python-Mundo 1/Desafios/DesafiosPycharm/Desafio006.py
+++
@@ -1,4 +0,0 @@
-n1 = int(input('Digite um valor Inteiro : '))
-s = n1 + 1
-a = n1 - 1
-print('O sucessor de {} é {}, e o antecessor de {}  é {}.'.format(n1,s,n1,a))
--- --Python (Curso Em video)--/Python-Mundo 1/Desafios/DesafiosPycharm/Desafio023.py
+++
@@ -1,3 +0,0 @@
-cid = input('Digite o nome de uma cidade: ')
-c = 'Santo' in cid
-print('Essa cidade começa ou tem Santo no nome? {}'.format(c))
