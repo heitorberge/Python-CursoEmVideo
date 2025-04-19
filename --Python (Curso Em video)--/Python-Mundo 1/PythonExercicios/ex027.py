130924581a9c1f880d419f347e0bce2897561be9
Heitor Berg <heitorberg114@gmail.com>
2025-04-19 10:02:34 -0300

    Add files via upload

---
+++ --Python (Curso Em video)--/Python-Mundo 1/PythonExercicios/ex027.py
@@ -0,0 +1,7 @@
+noco = input('Digite seu nome completo:  ').strip()
+nc = noco.split()
+pn = nc[0]
+un = nc[-1]
+print('Muito prazer em te Conhecer!')
+print('O seu Primeiro nome é {}'.format(pn))
+print('O seu Último nome é {}'.format(un))
