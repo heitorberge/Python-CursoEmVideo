8fb635726f147c4b44ce6e47d4527bc15e71f3db
Heitor Berg <heitorberg114@gmail.com>
2025-04-21 10:51:17 -0300

    Delete --Python (Curso Em video)--/Python-Mundo 1/PythonExercicios directory

--- --Python (Curso Em video)--/Python-Mundo 1/PythonExercicios/ex025.py
+++
@@ -1,2 +0,0 @@
-nome = str(input('Digite seu nome completo:  ')).strip().upper()
-print('Seu nome tem Silva?'.format('SILVA' in nome))
--- --Python (Curso Em video)--/Python-Mundo 1/PythonExercicios/ex027.py
+++
@@ -1,7 +0,0 @@
-noco = input('Digite seu nome completo:  ').strip()
-nc = noco.split()
-pn = nc[0]
-un = nc[-1]
-print('Muito prazer em te Conhecer!')
-print('O seu Primeiro nome é {}'.format(pn))
-print('O seu Último nome é {}'.format(un))
