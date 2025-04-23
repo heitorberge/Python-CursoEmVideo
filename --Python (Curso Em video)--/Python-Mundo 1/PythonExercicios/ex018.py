68080cf49bdb48b0a21949785ab8a90f02f2e6ca
Heitor Berg <heitorberg114@gmail.com>
2025-04-23 10:08:00 -0300

    Delete --Python (Curso Em video)--/Python-Mundo 1/PythonExercicios directory

--- --Python (Curso Em video)--/Python-Mundo 1/PythonExercicios/ex018.py
+++
@@ -1,8 +0,0 @@
-from math import radians, sin, cos, tan
-an = float(input("Digite algum ângulo em graus: "))
-se = sin(radians(an))
-co = cos(radians(an))
-ta = tan(radians(an))
-print("Seno: {:.2f}".format(se))
-print("Cosseno: {:.2f}".format(co))
-print("Tangente: {:.2f}".format(ta))
