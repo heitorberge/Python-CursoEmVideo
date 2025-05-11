265ec231449dff2d46556331bca9230b5a6c68de
Heitor Berg <heitorberg114@gmail.com>
2025-05-11 12:01:30 -0300

    Delete --Python (Curso Em video)--/Python-Mundo 2/Desafios directory

--- --Python (Curso Em video)--/Python-Mundo 2/Desafios/Desafio051.py
+++
@@ -1,8 +0,0 @@
-primeiro = int(input('Digite o primeiro termo: '))
-razao = int(input('Digite a razão da PA: '))
-print(primeiro, end=' -> ')
-atual = primeiro
-for i in range(0, 9):
-    atual = atual + razao
-    print(atual, end=' -> ')
-print('FIM')
