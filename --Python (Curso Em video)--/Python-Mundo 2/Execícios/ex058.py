7fb66a358c5ee33153e2d16ef0ad289c7a51c39e
Heitor Berg <heitorberg114@gmail.com>
2025-05-14 10:17:43 -0300

    Add files via upload

---
+++ --Python (Curso Em video)--/Python-Mundo 2/Execícios/ex058.py
@@ -0,0 +1,18 @@
+from random import randint
+computador = randint(0,10)
+print('Sou seu computador... Acabei de pensar em um número entre 0 e 10.')
+print('Será que você consegue adivinhar qual foi? ')
+acertou = False
+palpites = 0
+while not acertou:
+    jogador = int(input('Qual é seu palpite? '))
+    palpites += 1
+    if jogador == computador:
+        acertou = True
+    else:
+        if jogador < computador:
+            print('Tente um número Maior...')
+        elif jogador > computador:
+            print('Tente um número Menor...')
+print('Acertou!')
+print('Acertou com {} tentativas!'.format(palpites))
