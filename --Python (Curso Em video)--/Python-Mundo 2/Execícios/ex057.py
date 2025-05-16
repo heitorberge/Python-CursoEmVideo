9a8bd4ef932f2f74abdb3870cbcc6aed55928cef
Heitor Berg <heitorberg114@gmail.com>
2025-05-16 11:35:47 -0300

    Delete --Python (Curso Em video)--/Python-Mundo 2/Execícios directory

--- --Python (Curso Em video)--/Python-Mundo 2/Execícios/ex057.py
+++
@@ -1,4 +0,0 @@
-sexo = str(input('Informe seu sexo: [M/F] ')).strip().upper()[0]
-while sexo not in 'MmFf':
-    sexo = str(input('Dados inválidos.Por favor, Informe seu sexo: ')).strip().upper()[0]
-print('Sexo {} regisrado com sucesso'.format(sexo))
