bc2c5d1aacd54d99aa6286186343edf8674c7a34
Heitor Berg <heitorberg114@gmail.com>
2025-05-19 11:05:31 -0300

    Delete --Python (Curso Em video)--/Python-Mundo 2/Execícios directory

--- --Python (Curso Em video)--/Python-Mundo 2/Execícios/ex050.py
+++
@@ -1,8 +0,0 @@
-s = 0
-co = 0
-for c in range(0,6):
-    n = int(input('Digite o {}° valor: '.format(c)))
-    if n % 2 == 0:
-        s+=n
-        co+=1
-print('A soma de todos os {} valores pares digitados é {}'.format(co,s))
