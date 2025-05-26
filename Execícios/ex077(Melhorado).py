75b44304994f5825279c083e8f5b65d1739a125e
Heitor Berg <heitorberg114@gmail.com>
2025-05-26 20:15:53 -0300

    Add files via upload

---
+++ Execícios/ex077(Melhorado).py
@@ -0,0 +1,7 @@
+palavra = input('Digite uma palavra: ').strip()
+palavras = tuple(palavra.split())
+for palavra in palavras:
+    print(f'Na palavra "{palavra.upper()}" temos as vogais: ', end='')
+    for letra in palavra:
+        if letra.lower() in 'aáàãâeéèêiíìîoõóòôuúùû':
+            print(letra, end=' ')
