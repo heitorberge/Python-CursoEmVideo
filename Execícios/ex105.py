cc5cfdfe84a7bead613a5d352a77caa161686ae2
Heitor Berg <heitorberg114@gmail.com>
2025-06-20 21:21:21 -0300

    Add files via upload

---
+++ Execícios/ex105.py
@@ -0,0 +1,26 @@
+def notas(*n, sit=False):
+    """
+    -> Função para analisar notas e situações de vários alunos.
+    :param n: uma ou mais notas dos alunos (aceita várias)
+    :param sit: valor opcional, indicando se deve ou não adicionar a situação
+    :return: dicionário com várias informações sobre a situação da turma
+    """
+    r = dict()
+    r['total'] = len(n)
+    r['maior'] = max(n)
+    r['menor'] = min(n)
+    r['media'] = sum(n) / len(n)
+    if sit:
+        if r['media'] >= 7:
+            r['situacao'] = 'BOA'
+        elif r['media'] >= 5:
+            r['situacao'] = 'RAZOÁVEL'
+        else:
+            r['situacao'] = 'RUIM'
+    return r
+
+
+# Progama Principal
+resp = notas(8, 9, 10, 9.8, 10,10, sit=True)
+#print(resp)
+help(notas)
